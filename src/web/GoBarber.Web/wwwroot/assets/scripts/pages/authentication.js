4b3c9029f181522d66e426a885fd9b91582d4074
Fábio Maiorano <fsmaiorano@gmail.com>
2020-06-17 23:40:16 +0100

    wip css and scripts structure

---
+++ src/web/GoBarber.Web/wwwroot/assets/scripts/pages/authentication.js
@@ -0,0 +1,10 @@
+class Authentication {
+    constructor() {
+        this.init();
+    }
+    init() {
+        console.log("Authentication SCRIPTS!");
+    }
+}
+new Authentication();
+//# sourceMappingURL=authentication.js.map

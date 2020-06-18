1cf60fcebb5766a4f6917a2f646e99d090ec922e
Fábio <fsmaiorano@gmail.com>
2020-06-18 15:58:13 +0100

    add app project

---
+++ src/app/GoBarber.App/GoBarber.App/GoBarber.App/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GoBarber.App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/app/GoBarber.App/GoBarber.App/GoBarber.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GoBarber.App.Models;
+
+namespace GoBarber.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

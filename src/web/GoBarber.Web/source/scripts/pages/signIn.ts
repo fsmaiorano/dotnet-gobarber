b359c8a10ceaf19b697ed9d4d832ddfefe90f4bc
Fábio <fsmaiorano@gmail.com>
2020-06-19 23:08:14 +0100

    wip

---
+++ src/web/GoBarber.Web/source/scripts/pages/signIn.ts
@@ -0,0 +1,20 @@
+﻿class SignIn {
+  private btnDoLogin: HTMLButtonElement = document.querySelector(".btn-login");
+  private frmLogin: HTMLFormElement = document.querySelector(".form");
+  private input: HTMLInputElement = document.querySelector(".input");
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.btnDoLogin.addEventListener("click", (x) => {
+      debugger;
+    });
+    // this.btnDoLogin.addEventListener = () => {
+    //   console.log("click");
+    // };
+  }
+}
+
+new SignIn();
---
+++ src/web/GoBarber.Web/wwwroot/assets/scripts/pages/signin.js
@@ -0,0 +1,18 @@
+class SignIn {
+    constructor() {
+        this.btnDoLogin = document.querySelector(".btn-login");
+        this.frmLogin = document.querySelector(".form");
+        this.input = document.querySelector(".input");
+        this.init();
+    }
+    init() {
+        this.btnDoLogin.addEventListener("click", (x) => {
+            debugger;
+        });
+        // this.btnDoLogin.addEventListener = () => {
+        //   console.log("click");
+        // };
+    }
+}
+new SignIn();
+//# sourceMappingURL=signIn.js.map

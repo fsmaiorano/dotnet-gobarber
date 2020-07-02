239180d1b746a9b0bf0a2396f09dc909b21227e6
Fábio <fsmaiorano@gmail.com>
2020-07-02 14:03:27 +0100

    wip appointment info

--- src/web/GoBarber.Web/source/scripts/pages/appointment.ts
+++ src/web/GoBarber.Web/source/scripts/pages/appointment.ts
@@ -5,7 +5,49 @@
     }
 
     init() {
-        console.log("Appointments");
+        let today: Date = new Date();
+        let currentDay: number = today.getDate();
+        let currentMonth: number = today.getMonth();
+        let currentYear: number = today.getFullYear();
+
+        let url = `https://localhost:3333/appointment/appointmentlist?date=${currentDay}-${currentMonth + 1}-${currentYear}`;
+        fetch(url, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        }).then(function (response) {
+            return response.text();
+        })
+            .then(function (body) {
+                document.querySelector('.component-appointments').innerHTML = body;
+                let appointments: NodeListOf<HTMLDivElement> = document.querySelectorAll(".appointment");
+
+                for (let i = 0; i < appointments.length; i++) {
+                    appointments[i].addEventListener("click", ((event: MouseEvent) => {
+
+                        let id = event.currentTarget["id"];
+                        let url = `https://localhost:3333/appointment/appointmentdetail/${id}`;
+                        fetch(url, {
+                            method: "GET",
+                            headers: {
+                                Accept: "application/json",
+                                "Content-Type": "application/json",
+                            }
+                        }).then(function (response) {
+                            return response.text();
+                        }).then(function (body) {
+                            let appointmentList: HTMLDivElement = document.querySelector('.appointments-list');
+                            let appointmentDetail: HTMLDivElement = document.querySelector('.appointment-info');
+                            appointmentList.style.display = "none";
+                            debugger;
+                            appointmentDetail.innerHTML = body;
+                            appointmentDetail.style.display = "true";
+                        });
+                    }));
+                }
+            });
     }
 }
 new Appointment();
